d4b915824f67c3601b5b624c32f0bbc159cd1c73
Cesar Narvaez <Cesar.teacher24@gmail.com>
2023-02-09 02:42:53 -0500

    Other commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Portafolio } from './Components/Portafolio'
+import { Portafolio } from './components/Portafolio'
 import './style/style.css'
 
 
