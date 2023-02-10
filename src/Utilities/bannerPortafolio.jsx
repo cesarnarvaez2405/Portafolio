7de48d893cc4834aee625c00543b4adc935e600f
Cesar Narvaez <Cesar.teacher24@gmail.com>
2023-02-09 20:16:34 -0500

    Commit ImgPortafolio

--- src/Utilities/bannerPortafolio.jsx
+++ src/Utilities/bannerPortafolio.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from '../style/banner.module.css'
-import image from "../Content/imgPortafolio.jpeg";
+import image from "./imgPortafolio.jpeg";
 
 export const BannerPortafolio = () => {
     return (
