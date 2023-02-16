0c157766027f48d83a94366d6eb9e75230369747
Cesar Narvaez <Cesar.teacher24@gmail.com>
2023-02-15 23:54:58 -0500

    Commit 3

--- src/Utilities/bannerPortafolio.jsx
+++ src/Utilities/bannerPortafolio.jsx
@@ -3,12 +3,16 @@ import style from '../style/banner.module.css'
 import image from "../Content/imgPortafolio.jpeg";
 
 export const BannerPortafolio = () => {
+
+    // const image = require('../Content/imgPortafolio.jpeg');
     return (
+
         <>
             <div className= {style.containerBanner}>
                 <div class="animate__animated animate__fadeInDown" className= {style.container}>
                     <div className= {style.infoBanner}>
                         <img src= {image} alt="Imagen Portafolio" className= {style.imgPortafolio} />
+                        {/* <img src={require('../Content/imgPortafolio.JPEG')} className= {style.imgPortafolio} alt="Imagen Portafolio"  /> */}
                         <div>
                             <h1 className={style.cesarNarvaez}>Soy Cesar Narvaez</h1>
                             <p className={style.frontend}>Desarrollador Frontend <a className={style.spaceinvader}></a></p>
