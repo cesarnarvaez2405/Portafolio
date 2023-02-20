e3603ff74c44223e699c8b68f961426a4a20e291
Cesar Narvaez <Cesar.teacher24@gmail.com>
2023-02-20 18:31:04 -0500

    Commit responsive Restante

--- src/Utilities/sobreMi.jsx
+++ src/Utilities/sobreMi.jsx
@@ -16,30 +16,23 @@ export const SobreMi = () => {
         <>
             <div className={style.conteiner}>
                 <div className={style.conteinerSobreMi}>
-                    <div className={style.shape}>
-                        {/* <svg viewBox="0 0 480 480" xmlns="http://www.w3.org/2000/svg" >
-                            <g>
-                                <path fill="#474bff" d="M396,297.5Q377,355,323,380.5Q269,406,202.5,413Q136,420,101,361.5Q66,303,72.5,242Q79,181,100,107.5Q121,34,198.5,35.5Q276,37,315,90.5Q354,144,384.5,192Q415,240,396,297.5Z" />
-                            </g>
-                        </svg> */}
-                    </div>
                     <div className="animate__animated animate__fadeInLeft">
                         <div className={style.conteinerContenido}>
                             <div className={style.contenidoSobreMi}>
                                 <h1 className={style.titulo}>Sobre mi</h1>
                                 <p className={style.parrafo}>Desarrollador junior Frontend con
-2 años de experiencia en
-desarrollo web en diferentes
-tecnologías. Apasionado de la
-programación con sólidos
-conocimientos de programación
-(Html 5, css 3, Typescript,
-Javascript, React, SQL, Git).
-Mi capacidad de generar ideas y
-mi facilidad para trabajar en
-equipos son mis principales
-herramientas para trabajar y
-crecer como desarrollador.</p>
+                                    2 años de experiencia en
+                                    desarrollo web en diferentes
+                                    tecnologías. Apasionado de la
+                                    programación con sólidos
+                                    conocimientos de programación
+                                    (Html 5, css 3, Typescript,
+                                    Javascript, React, SQL, Git).
+                                    Mi capacidad de generar ideas y
+                                    mi facilidad para trabajar en
+                                    equipos son mis principales
+                                    herramientas para trabajar y
+                                    crecer como desarrollador.</p>
                             </div>
                         </div>
                     </div>
